fd25e50c9f0248d08c02b4c45cf12c66ad0733cf
Yuumi Yoshida <yy@ey-office.com>
2020-07-07 11:54:11 +0900

    Use react-hook-form and validation

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import TextField from '@material-ui/core/TextField'
+import Alert from '@material-ui/lab/Alert'
 import { Paper, Grid, Fab, Container, Box } from '@material-ui/core'
 
 const Bmi: React.FC = () => {
-  const [height, setHeight] = useState("")
-  const [weight, setWeight] = useState("")
   const [judgeText, setJudgeText] = useState("")
   const [judgeColor, setJudgeColor] = useState("white")
 
-  const judgeBMI = () => {
-    const heightM = Number(height) / 100
-    const bmi = Number(weight) / (heightM ** 2)
+  const { handleSubmit, register, errors } = useForm()
+
+  type FormValues = {
+    height: string
+    weight: string
+  }
+
+  const judgeBMI: SubmitHandler<FormValues> = (data) => {
+    console.log(data)
+
+    const heightM = Number(data.height) / 100
+    const bmi = Number(data.weight) / (heightM ** 2)
 
     if (bmi < 18.5) {
       setJudgeText("やせ")
@@ -25,33 +34,38 @@ const Bmi: React.FC = () => {
     }
   }
 
-  const boxStyle = {width: 100, height: 60, "line-height" : "60px", "text-align": "center"}
-
+  const boxStyle = {width: 100, height: 60, lineHeight : "60px", textAlign: "center" as "center"}
+  const numberInputValidation = {
+    required: "必須項目です",
+    pattern: {value: /^\d+$/, message: "数字のみ入力できます"}
+  }
   return (
     <Container maxWidth="xs">
+      <form onSubmit={handleSubmit(judgeBMI)}>
       <Grid container spacing={3}>
         <Grid item xs={6}>
-          <TextField id="inputHeight" label="身長"
-            value={height} placeholder="170"
-            onChange={(e) => setHeight(e.target.value)} />
+          <TextField id="inputHeight" label="身長" placeholder="170"
+            name="height" inputRef={register(numberInputValidation)} />
         </Grid>
         <Grid item xs={6}>
-          <TextField id="inputWeight" label="体重"
-            value={weight} placeholder="60"
-            onChange={(e) => setWeight(e.target.value)} />
+          <TextField id="inputWeight" label="体重" placeholder="60"
+            name="weight" inputRef={register(numberInputValidation)} />
         </Grid>
       </Grid>
+      {errors.height && <Alert severity="error">身長は{errors.height.message}</Alert>}
+      {errors.weight && <Alert severity="error">体重は{errors.weight.message}</Alert>}
       <Box style={{height: 50}}></Box>
       <Grid container spacing={10}>
         <Grid item xs={2}></Grid>
         <Grid item xs={2}>
-          <Fab  color="primary" onClick={() => judgeBMI()}>BMI</Fab>
+          <Fab  color="primary" type="submit">BMI</Fab>
         </Grid>
         <Grid item xs={6}>
           {judgeText &&
           <Paper style={{...boxStyle, backgroundColor: judgeColor}}>{judgeText}</Paper>}
         </Grid>
       </Grid>
+      </form>
     </Container>
   )
 }
